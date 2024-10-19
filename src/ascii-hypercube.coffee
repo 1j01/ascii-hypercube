1j01/ascii-hypercube

commit 3e955d3d76fb5b5e8b8903b5e0089f26ef381095

Split core algorithm out of app

src/ascii-hypercube.coffee
@@ -1,0 +1,88 @@
+
+grid = []
+
+n_overlaps = 0
+overlaps = {}
+
+plot_glyph = (glyph, x, y)->
+	if grid.length <= y
+		console?.warn? "Expanding vertically"
+		for i in [grid.length..y]
+			grid[i] = []
+	if grid[y].length < x
+		for i in [grid[y].length..x]
+			grid[y][i] = " "
+	existing = grid[y][x]
+	if existing? and existing isnt " " and existing isnt glyph
+		overlaps[existing] ?= {}
+		overlaps[existing][glyph] ?= 0
+		overlaps[existing][glyph] += 1
+		n_overlaps += 1
+	grid[y][x] = glyph
+
+plot_hypercube_vertices = (dimensions, glyph, x, y)->
+
+	[further_dimensions..., _] = dimensions
+
+	if further_dimensions.length
+		dimension = further_dimensions[further_dimensions.length - 1]
+		plot_hypercube_vertices(further_dimensions, glyph, x, y)
+		plot_hypercube_vertices(further_dimensions, glyph,
+			x + dimension.length * dimension.x_per_glyph
+			y + dimension.length * dimension.y_per_glyph
+		)
+	else
+		plot_glyph(glyph, x, y)
+
+plot_hypercube = (dimensions, x, y)->
+
+	[further_dimensions..., dimension] = dimensions
+	{length, x_per_glyph, y_per_glyph, glyphs} = dimension
+	
+	if length > 0 and glyphs.length > 0
+		for i in [1..length]
+			glyph = glyphs[i % glyphs.length]
+			plot_hypercube_vertices(dimensions, glyph, x + i * x_per_glyph, y + i * y_per_glyph)
+	if length > 0
+		if further_dimensions.length
+			plot_hypercube(further_dimensions, x, y)
+			plot_hypercube(further_dimensions, x + length * x_per_glyph, y + length * y_per_glyph)
+		else if glyphs.length > 0
+			plot_glyph(glyphs[0], x, y)
+	else
+		# skip this dimension for the sake of overlap counting mainly
+		plot_hypercube(further_dimensions, x, y)
+
+render_hypercube = (dimensions, splitter)->
+	x = 0
+	y = 0
+	width = 0
+	height = 0
+	for dimension in dimensions
+		width = Math.max(width, width + dimension.length * dimension.x_per_glyph)
+		height = Math.max(height, height + dimension.length * dimension.y_per_glyph)
+		x = Math.max(x, x - dimension.length * dimension.x_per_glyph)
+		y = Math.max(y, y - dimension.length * dimension.y_per_glyph)
+	width += x
+	height += y
+	grid =
+		for [0..height]
+			[]
+			# for [0..width]
+			# 	" "
+	
+	n_overlaps = 0
+	overlaps = {}
+
+	for dimension in dimensions
+		dimension.glyphs ?= (splitter ?= new GraphemeSplitter()).splitGraphemes(dimension.text)
+
+	plot_hypercube(dimensions, x, y)
+	
+	output_text_art =
+		(line.join("") for line in grid).join("\n")
+
+	return { text: output_text_art, overlaps, numOverlaps: n_overlaps }
+
+# TODO: ESM / CommonJS export
+window.render_hypercube = render_hypercube

index.html
@@ -203,6 +203,7 @@
 	</p>
 </footer>
 
+<script src="./src/ascii-hypercube.coffee" type="text/coffeescript"></script>
 <script src="./src/app.coffee" type="text/coffeescript"></script>
 
 </body>

src/app.coffee
@@ -1,58 +1,5 @@
-
-grid = []
-
-n_overlaps = 0
-overlaps = {}
-
-plot_glyph = (glyph, x, y)->
-	if grid.length <= y
-		console?.warn? "Expanding vertically"
-		for i in [grid.length..y]
-			grid[i] = []
-	if grid[y].length < x
-		for i in [grid[y].length..x]
-			grid[y][i] = " "
-	existing = grid[y][x]
-	if existing? and existing isnt " " and existing isnt glyph
-		overlaps[existing] ?= {}
-		overlaps[existing][glyph] ?= 0
-		overlaps[existing][glyph] += 1
-		n_overlaps += 1
-	grid[y][x] = glyph
-
-plot_hypercube_vertices = (dimensions, glyph, x, y)->
-
-	[further_dimensions..., _] = dimensions
-
-	if further_dimensions.length
-		dimension = further_dimensions[further_dimensions.length - 1]
-		plot_hypercube_vertices(further_dimensions, glyph, x, y)
-		plot_hypercube_vertices(further_dimensions, glyph,
-			x + dimension.length * dimension.x_per_glyph
-			y + dimension.length * dimension.y_per_glyph
-		)
-	else
-		plot_glyph(glyph, x, y)
-
-plot_hypercube = (dimensions, x, y)->
-
-	[further_dimensions..., dimension] = dimensions
-	{length, x_per_glyph, y_per_glyph, glyphs} = dimension
-	
-	if length > 0 and glyphs.length > 0
-		for i in [1..length]
-			glyph = glyphs[i % glyphs.length]
-			plot_hypercube_vertices(dimensions, glyph, x + i * x_per_glyph, y + i * y_per_glyph)
-	if length > 0
-		if further_dimensions.length
-			plot_hypercube(further_dimensions, x, y)
-			plot_hypercube(further_dimensions, x + length * x_per_glyph, y + length * y_per_glyph)
-		else if glyphs.length > 0
-			plot_glyph(glyphs[0], x, y)
-	else
-		# skip this dimension for the sake of overlap counting mainly
-		plot_hypercube(further_dimensions, x, y)
-
+# import {render_hypercube} from "./ascii-hypercube.js"
+# render_hypercube = await import("./ascii-hypercube.js").then(m => m.render_hypercube)
 
 form = document.getElementById("inputs")
 add_dimension_button = document.getElementById("add-dimension")
@@ -63,14 +10,13 @@ overlap_summary = document.getElementById("overlap-summary")
 overlap_details = document.getElementById("overlap-details")
 copied_indicator = document.getElementById("copied-to-clipboard")
 
-output_text_art = ""
-update_output_area = ->
+update_output_area = (output)->
 	if markdown_format_checkbox.checked
-		output_textarea.value = output_text_art.split("\n").map(
+		output_textarea.value = output.text.split("\n").map(
 			(str)-> str.replace(/^/g,"    ")
 		).join("\n")
 	else
-		output_textarea.value = output_text_art
+		output_textarea.value = output.text
 	
 	style = window.getComputedStyle(output_textarea, null)
 	padding_top = parseFloat(style.getPropertyValue("padding-top"))
@@ -84,15 +30,15 @@ update_output_area = ->
 	output_textarea.style.height = "#{scroll_height + padding_top + padding_bottom}px"
 	
 	overlap_summary.textContent =
-		if n_overlaps > 0
-			"🙈 #{n_overlaps} glyphs occlude differing glyphs"
+		if output.numOverlaps > 0
+			"🙈 #{output.numOverlaps} glyphs occlude differing glyphs"
 		else
 			"👌 all overlapping glyphs match"
 
 	overlap_details.innerHTML =
-		if n_overlaps > 0
+		if output.numOverlaps > 0
 			"<ul>#{
-				(for under, overs of overlaps
+				(for under, overs of output.overlaps
 					(for over, n_over of overs
 						"<li><code>#{over}</code> over <code>#{under}</code> (#{n_over})</li>"
 					).join("\n")
@@ -115,35 +61,12 @@ dimensions = [
 
 do compute = ->
 	
-	x = 0
-	y = 0
-	width = 0
-	height = 0
-	for dimension in dimensions
-		width = Math.max(width, width + dimension.length * dimension.x_per_glyph)
-		height = Math.max(height, height + dimension.length * dimension.y_per_glyph)
-		x = Math.max(x, x - dimension.length * dimension.x_per_glyph)
-		y = Math.max(y, y - dimension.length * dimension.y_per_glyph)
-	width += x
-	height += y
-	grid =
-		for [0..height]
-			[]
-			# for [0..width]
-			# 	" "
-	
-	n_overlaps = 0
-	overlaps = {}
+	# for dimension in dimensions
+	# 	dimension.glyphs = splitter.splitGraphemes(dimension.text)
 
-	for dimension in dimensions
-		dimension.glyphs = splitter.splitGraphemes(dimension.text)
+	output = render_hypercube(dimensions, splitter)
 
-	plot_hypercube(dimensions, x, y)
-	
-	output_text_art =
-		(line.join("") for line in grid).join("\n")
-	
-	update_output_area()
+	update_output_area(output)
 
 make_dimension_row = (nd_name, dimension)->
 	container_el = document.createElement("p")
@@ -219,7 +142,8 @@ add_dimension_button.addEventListener "click", ->
 for input in document.querySelectorAll("form input")
 	input.addEventListener("input", compute)
 
-markdown_format_checkbox.addEventListener("change", update_output_area)
+# Optimization: could retain output and only call update_output_area
+markdown_format_checkbox.addEventListener("change", compute)
 
 tid = -1
 show_copied_indicator = ->

commit a47888dbd7aa074f9d7c9df32d48edaeaf8e9dee

Fix updating text for a dimension in the app
Broken in 3e955d3d76fb5b5e8b8903b5e0089f26ef381095

src/ascii-hypercube.coffee
@@ -75,7 +75,10 @@ renderHypercube = (dimensions, splitter)->
 	overlaps = {}
 
 	for dimension in dimensions
-		dimension.glyphs ?= (splitter ?= new (window?.GraphemeSplitter ? require('grapheme-splitter'))()).splitGraphemes(dimension.text)
+		# Not using `dimension.glyphs ?= ...` because if the objects are reused, the glyphs would be reused too (cached forever)
+		# TODO: don't modify the input objects at all
+		if dimension.text?
+			dimension.glyphs = (splitter ?= new (window?.GraphemeSplitter ? require('grapheme-splitter'))()).splitGraphemes(dimension.text)
 
 	if dimensions.length > 0
 		plotHypercube(dimensions, x, y)